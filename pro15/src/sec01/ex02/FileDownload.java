4312f381347d65613e7199a2414f2afb2109c446
bexter1999 <bexter125@naver.com>
2022-09-28 17:47:48 +0900

    myJSP

--- pro15/src/sec01/ex02/FileDownload.java
+++ pro15/src/sec01/ex02/FileDownload.java
@@ -13,38 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/download.do")
 public class FileDownload extends HttpServlet {
-	
-	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doHandle(request, response);
 	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 		doHandle(request, response);
 	}
-	
-	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
 		String file_repo = "C:\\file_repo";
 		String fileName = (String) request.getParameter("fileName");
 		System.out.println("fileName=" + fileName);
-		OutputStream out = response.getOutputStream();
-		
+		OutputStream out = response.getOutputStream();	
+
 		String downFile = file_repo + "\\" + fileName;
 		File f = new File(downFile);
 		response.setHeader("Cache-Control", "no-cache");
-		response.addHeader("Content-disposition","attachment; fileName=" + fileName);
+		response.addHeader("Content-disposition", "attachment; fileName=" + fileName);
 		FileInputStream in = new FileInputStream(f);
 		byte[] buffer = new byte[1024 * 8];
-		while(true) {
+		while (true) {
 			int count = in.read(buffer);
-			if(count == -1)
+			if (count == -1)
 				break;
 			out.write(buffer, 0, count);
 		}
 		in.close();
-		out.close();
+		out.close();		
 	}
 }
